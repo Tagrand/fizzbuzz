70e511ba5c98981baf8104f52c611e5b25d0f08e
Benjamin Pourian <b.pourian@hotmail.co.uk>
2017-12-19 17:24:12 +0000

    updated method to the Integer class

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -1,18 +1,22 @@
-def fizzbuzz(n)
-   return "fizzbuzz" if n % 15 == 0
-   return "fizz" if n % 3 == 0
-   return "buzz" if n % 5 == 0
-   n.to_s
+class Integer
+
+  def fizzbuzz
+     return "fizzbuzz" if self % 15 == 0
+     return "fizz" if self % 3 == 0
+     return "buzz" if self % 5 == 0
+     self.to_s
+end
+
 end
 
 
 =begin
 
-Alternative, one line solution	
+Alternative, one line solution
 
 def fizzbuzz(n)
-   n % 3 == 0 ?  (n % 5 == 0 ? "fizzbuzz"  : "fizz") : (n % 5 == 0 ? "buzz"  : n.to_s) 
+   n % 3 == 0 ?  (n % 5 == 0 ? "fizzbuzz"  : "fizz") : (n % 5 == 0 ? "buzz"  : n.to_s)
 end
 
-	
+
 =end
